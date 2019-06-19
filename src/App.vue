9f69be70fc5a8d1f35ad192a8332e217e641557a
Laura Sheach <laura_bubble_git@yahoo.com>
2019-06-19 16:15:34 +0100

    First two items of MVP complete

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Countries of the World</h1>
+      <div class="main-container">
+        <countries-list :countries='countries'></countries-list>
+        <country-detail :country='selectedCountry'></country-detail>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {eventBus} from './main.js';
+import CountriesList from './components/CountriesList.vue';
+import CountryDetail from './components/CountryDetail.vue';
 export default {
   name: 'app',
+  data(){
+    return {
+      countries: [],
+      selectedCountry: null
+    };
+  },
   components: {
-    HelloWorld
+    "countries-list": CountriesList,
+    "country-detail": CountryDetail
+  },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(result => result.json())
+    .then(countries => this.countries = countries);
+    eventBus.$on('country-selected', (country) => {
+      this.selectedCountry = country;
+    })
   }
 }
 </script>
---
+++ src/components/CountryItem.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <ul>
+      <li v-on:click="handleClick">{{country.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {eventBus} from '../main.js'
+
+export default {
+  name: 'country-item',
+  props: ['country'],
+  methods: {
+    handleClick(){
+      eventBus.$emit('country-selected', this.country)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
